a0a7aaf25c2e97061840e8fdee91621c52263163
Christopher Fariss <cjf0006@gmail.com>
2023-01-20 21:47:33 -0500

    copy edits and name changes

--- R-Program-Challenges/R_Challenge_simulation_dice_differences.R
+++ R-Program-Challenges/R_Challenge_simulation_dice_differences.R
@@ -1,4 +1,4 @@
-## R_Challenge_probability_dice_differences.R
+## R_Challenge_simulation_dice_differences.R
 ##########################################################################
 ## INSTRUCTOR: Christopher Fariss
 ##
