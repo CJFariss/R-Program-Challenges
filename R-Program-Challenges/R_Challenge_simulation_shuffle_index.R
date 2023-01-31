357a825612a308b2d8ec96e8fc8a775f471ead4f
Christopher Fariss <cjf0006@gmail.com>
2023-01-31 12:37:34 -0500

    Update R_Challenge_simulation_shuffle_index.R

--- R-Program-Challenges/R_Challenge_simulation_shuffle_index.R
+++ R-Program-Challenges/R_Challenge_simulation_shuffle_index.R
@@ -30,15 +30,27 @@
 ##
 ## Hint: If you are having trouble thinking through any of the steps of this challenge, imagine what happens when shuffling a deck of cards. The goal of this challenge is to write a program that replicates the physical process of shuffling a deck of cards. Instead of using cards, we will use a vector of index values.
 ##
-## (1) generate an index of length 10 (or any other even number)
-## (2) calculate the cut_point, the coordinate of the index less than the median point (for an odd length index, the coordinate is the median)
-## (3) using the cut_point coordinate, take the top and bottom halves of the index and shuffle them so that the first index value of the top half enters the first position of a new vector object followed by the first index value of the second half. Repeat this process until you have added every one of the coordinate positions from both halves into the new vector. The new vector is a shuffled vector of the original index coordinates
-## (4) setup a function or program to repeat this process for n number of times
-## (5) alternate which subset of the index vector is first added to the shuffled vector; i.e., start with the top half and then the bottom half of the index for the odd number of n iterations and the bottom half and the top half of the index for the even number of n iterations
-## (6) adjust the function so that it can handle vectors that are of odd lengths
-## (7) randomly select a times argument for the function (i.e., the number of times the program shuffles the deck) without using one of the random number generators available in R (hint: try using the Sys.time() function)
-## (8) compare the output of the function to the sample() function (but don't use the sample function to accomplish any of the steps above)
-## (9) use the set.seed() function to control the exact output of the sample() function in (8) above
+## (1) Generate an index of length 10 (or any other even number).
+##
+## (2) Calculate the cut_point, the coordinate of the index less than the median point (for an odd length index, the coordinate is the median).
+##
+## (3) Using the cut_point coordinate, take the top and bottom halves of the index and shuffle them so that the first index value of the top half enters the first position of a new vector object followed by the first index value of the second half. Repeat this process until you have added every one of the coordinate positions from both halves into the new vector. The new vector is a shuffled vector of the original index coordinates.
+##
+## (4) Setup a function or program to repeat this process for n number of times.
+##
+## (5) Alternate which subset of the index vector is first added to the shuffled vector; i.e., start with the top half and then the bottom half of the index for the odd number of n iterations and the bottom half and the top half of the index for the even number of n iterations.
+##
+## (6) Adjust the function so that it can handle vectors that are of odd lengths.
+##
+## Warning: the next few steps are more challanging: 
+##
+## (7) Randomly select a times argument for the function (i.e., the number of times the program shuffles the deck) without using one of the random number generators available in R (hint: try using the Sys.time() function).
+##
+## (8) Compare the output of the function to the sample() function (but don't use the sample function to accomplish any of the steps above).
+##
+## (9) Use the set.seed() function to control the exact output of the sample() function in (8) above
+##
+## (10) Create a simulation to check if the output from your shuffle function is random.
 ##
 ##########################################################################
 
