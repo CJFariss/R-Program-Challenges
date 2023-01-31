f50c667021dc2ad8bdbab706cd167578995a952d
Christopher Fariss <cjf0006@gmail.com>
2023-01-31 12:37:31 -0500

    Update R_Challenge_simulation_high_card_draw.R

--- R-Program-Challenges/R_Challenge_simulation_high_card_draw.R
+++ R-Program-Challenges/R_Challenge_simulation_high_card_draw.R
@@ -8,7 +8,7 @@
 ## COURSE NAME: Advanced Computational Methods for Social Media and Textual Data (2F)
 ## University of Essex Summer School 2022, 2021, 2020
 ##
-## Date: 2022-05-17
+## Date: 2023-01-31
 ##
 ## Please e-mail me if you find any errors or have and suggestions (either email is fine)
 ## e-mail: cjf0006@gmail.com
@@ -28,15 +28,23 @@
 ##
 ## Steps for the Challenge:
 ##
-## (1) as a character vector, define a deck or cards that ranges from "2" to "Ace" so that all of the standard cards are present as character strings (don't worry about suits, so just 13 cards)
-## (2) generate another vector that contains the coordinates for the character vector created in step (1), this will also be of length 13
-## (3) program a draw of 5 cards from the deck of cards vector at random (hint: use the sample function in the base library or your shuffle function)
-## (4) program two draws of cards to player1 and player2 (hint: use 10 of the 13 cards, for 5 cards in player1's deck and player2's deck)
-## (5) determine with a logical statement which player has the highest card
-## (6) simulate a series of games between player1 and player2, who wins the most often?, does either player have an advantage?
-## (7) write a function to accomplish (1) - (5)
-## (8) write a separate function to accomplish (6)
-## (9) combine the functions created in (7) and (8)
+## (1) As a character vector, define a deck or cards that ranges from "2" to "Ace" so that all of the standard cards are present as character strings (don't worry about suits, so just 13 cards).
+##
+## (2) Generate another vector that contains the coordinates for the character vector created in step (1), this will also be of length 13.
+##
+## (3) Program a draw of 5 cards from the deck of cards vector at random (hint: use the sample function in the base library or your shuffle function).
+##
+## (4) Program two draws of cards to player1 and player2 (hint: use 10 of the 13 cards, for 5 cards in player1's deck and player2's deck).
+##
+## (5) Determine with a logical statement which player has the highest card.
+##
+## (6) Simulate a series of games between player1 and player2, who wins the most often?, does either player have an advantage?
+##
+## (7) Write a function to accomplish (1) - (5).
+##
+## (8) Write a separate function to accomplish (6).
+##
+## (9) Combine the functions created in (7) and (8).
 ##
 ##########################################################################
 
