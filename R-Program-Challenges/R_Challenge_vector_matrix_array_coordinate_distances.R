9f835544906e748d6e1d1b7f61970e0fa34eadaf
Christopher Fariss <cjf0006@gmail.com>
2025-01-14 20:27:38 -0500

    Update R_Challenge_vector_matrix_array_coordinate_distances.R

--- R-Program-Challenges/R_Challenge_vector_matrix_array_coordinate_distances.R
+++ R-Program-Challenges/R_Challenge_vector_matrix_array_coordinate_distances.R
@@ -39,6 +39,8 @@
 ##
 ## (5) Generalize (4) for an array of 3 dimensions. The distance between two cells in a 3-D array with coordinates (x1, y1, z1) and (x2, y2, z2) is the square root of the squared distances: sqrt((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2).
 ##
+## warning: it's going to be even more challenging below.
+##
 ## (6) Write a function for (1), (4), and (5).
 ##
 ## (7) Write a function to generalize this process for an array of any dimensions.
