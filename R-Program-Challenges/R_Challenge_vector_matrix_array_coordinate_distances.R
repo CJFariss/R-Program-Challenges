07a1b3101874cad7efcb068e520cba386bbc22ac
Christopher Fariss <cjf0006@gmail.com>
2025-01-14 20:09:20 -0500

    Update R_Challenge_vector_matrix_array_coordinate_distances.R

--- R-Program-Challenges/R_Challenge_vector_matrix_array_coordinate_distances.R
+++ R-Program-Challenges/R_Challenge_vector_matrix_array_coordinate_distances.R
@@ -29,13 +29,13 @@
 ##
 ## Steps for the Challenge
 ##
-## (1) Generate a 1-D vector x of arbitrary length.
+## (1) Generate a 1-D vector (1-dimensional) object of arbitrary length.
 ##
 ## (2) Choose a position in the vector (coordinate).
 ##
 ## (3) Generate a new vector with the distances from the chosen position in the vector to every other element in the vector. The distance formula we will use is sqrt((position_vector - coordinate)^2).
 ##
-## (4) Repeat steps 1-3 for a 2-D matrix xy with dimensions x and y. The distance between two cells in a matrix with coordinates (x1, y1) and (x2, y2) is the square root of the squared distances (hint use the sqrt() function): sqrt((x1 - x2)^2 + (y1 - y2)^2).
+## (4) Repeat steps 1-3 for a 2-D matrix (2-dimensional) object with dimensions for rows and columns. I'll refer to the rows as x and columns as y in the rest of this step's instructions. The distance between two cells in a matrix with coordinates (x1, y1) and (x2, y2) is the square root of the squared distances (hint use the sqrt() function): sqrt((x1 - x2)^2 + (y1 - y2)^2).
 ##
 ## (5) Generalize (4) for an array of 3 dimensions. The distance between two cells in a 3-D array with coordinates (x1, y1, z1) and (x2, y2, z2) is the square root of the squared distances: sqrt((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2).
 ##
